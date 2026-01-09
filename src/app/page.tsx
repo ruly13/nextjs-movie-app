798bf66ef263778be68e70520fdc92f63fd6b2d4
Rohmat Choiruly Anwar <rohmatchoirulyanwar@gmail.com>
2026-01-09 15:38:21 +0700

    add page

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import { Navbar } from "@/container/shared/Navbar";
+import { HomePage } from "@/container/home/HomePage";
+import { Footer } from "@/container/shared/Footer";
+
+interface HomeProps {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const resolvedSearchParams = await searchParams;
+  const trendingTimeWindow = (resolvedSearchParams.trending as "day" | "week") || "day";
+
+  return (
+    <main className="min-h-screen bg-background relative flex flex-col">
+      <Navbar />
+      <HomePage timeWindow={trendingTimeWindow} />
+      <Footer />
+    </main>
+  );
+}
